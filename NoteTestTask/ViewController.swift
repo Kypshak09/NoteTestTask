33958f3870a42a6f5f3dc54ffeb6766e4f548aa6
Kypshak09 <amirzhunusov09@gmail.com>
2023-04-06 12:00:37 +0600

    Initial Commit

---
+++ NoteTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteTestTask
+//
+//  Created by Amir Zhunussov on 06.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
