128178ca182a0f355a898f1f1b091e7bd0b40b33
Kypshak09 <amirzhunusov09@gmail.com>
2023-04-11 13:37:00 +0600

    fixed bug with pin

--- NoteTestTask/Controller/NotesViewController.swift
+++ NoteTestTask/Controller/NotesViewController.swift
@@ -13,6 +13,7 @@ import RealmSwift
 class NotesViewController: TableNotesView {
 
     var data: Results<NoteData>!
+    var sortedNotes: [NoteData] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,7 @@ class NotesViewController: TableNotesView {
     override func viewWillAppear(_ animated: Bool) {
         data = RealmManager.shared.localRealm.objects(NoteData.self)
         collectionView.reloadData()
+        updateSortedNotes()
         updateVisiability()
     }
     
@@ -40,17 +42,16 @@ class NotesViewController: TableNotesView {
             }
         }
     }
-    private func sortedData() -> [NoteData] {
-        guard let unsortedData = data else { return [] }
-
-        let unsortedArray = Array(unsortedData)
-        return unsortedArray.sorted { (first, second) in
-                    if first.pinnedNote != second.pinnedNote {
-                        return first.pinnedNote && !second.pinnedNote
-                    } else {
-                        return first.editedData! > second.editedData!
-                    }
-                }
+    
+    private func updateSortedNotes() {
+        guard let unsortedData = data else {return}
+        sortedNotes = Array(unsortedData).sorted { (first, second) in
+            if first.pinnedNote != second.pinnedNote {
+                return first.pinnedNote && !second.pinnedNote
+            } else {
+                return first.editedData! > second.editedData!
+            }
+        }
     }
     
     private func configureButton() {
@@ -64,18 +65,20 @@ class NotesViewController: TableNotesView {
     @objc func deleteNote(sender: UIButton) {
         guard let cell = sender.superview?.superview as? CollectionViewCell,
             let indexPath = collectionView.indexPath(for: cell) else { return }
-            let note = data[indexPath.item]
-            RealmManager.shared.deleteData(data: note)
-            collectionView.deleteItems(at: [indexPath])
+        let note = sortedNotes[indexPath.row]
+        RealmManager.shared.deleteData(data: note)
+        updateSortedNotes()
+        collectionView.reloadData()
         updateVisiability()
     }
     
     @objc func togglePinNote(sender: UIButton) {
         guard let cell = sender.superview?.superview as? CollectionViewCell,
               let indexPath = collectionView.indexPath(for: cell) else {return}
-        let note = sortedData()[indexPath.item]
+        let note = sortedNotes[indexPath.item]
         let pinned = !note.pinnedNote
         RealmManager.shared.updateData(data: note, withTitle: nil, description: nil, color: nil, tag: nil, pinned: pinned)
+        updateSortedNotes()
         collectionView.reloadData()
     }
     
@@ -98,12 +101,12 @@ class NotesViewController: TableNotesView {
 
 extension NotesViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sortedData().count
+        return sortedNotes.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! CollectionViewCell
-        let note = sortedData()[indexPath.item]
+        let note = sortedNotes[indexPath.item]
         cell.buttonPin.addTarget(self, action: #selector(togglePinNote), for: .touchUpInside)
         cell.buttonDelete.addTarget(self, action: #selector(deleteNote), for: .touchUpInside)
         cell.configureData(note: note)
@@ -112,8 +115,12 @@ extension NotesViewController: UICollectionViewDelegate, UICollectionViewDataSou
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let editNoteVC = EditNoteViewController()
-        editNoteVC.noteData = sortedData()[indexPath.item]
+        editNoteVC.noteData = sortedNotes[indexPath.item]
         navigationController?.pushViewController(editNoteVC, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 156, height: 212)
     }
 }
 
